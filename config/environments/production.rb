0e1b6c16e4830dd3bc20b8518422760832a547a2
Paul Jensen <paulbjensen@Paul-Jensens-MacBook.local>
2010-03-22 22:43:17 +0000

    fix for s3 keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,5 +2,3 @@ config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
-S3_KEY    = '1NYPX0J3FR6JWHF2YN02'
-S3_SECRET = '58Qu+kiOk76R4YhU7fRhEvkhjc5GFB79w1YACwBw'
